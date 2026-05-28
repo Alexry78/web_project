36e6d97f9baf2e782efe9c12b1874960affef2ac
Alexry78 <144147456+Alexry78@users.noreply.github.com>
2026-05-28 16:42:33 +0300

    Task 8 ready: add contact form with feedback

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from pages.views import index, about, movie_detail   
 from django.conf import settings
 from django.conf.urls.static import static
+from pages.views import index, about, movie_detail, contact
 
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('movie/<int:pk>/', movie_detail, name='movie_detail'),   
+    path('movie/<int:pk>/', movie_detail, name='movie_detail'), 
+    path('contact/', contact, name='contact'),  
     path('admin/', admin.site.urls),
 ]
 
--- pages/views.py
+++ pages/views.py
@@ -1,18 +1,33 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie
+from .forms import FeedbackForm
 
 def index(request):
-    movies = Movie.objects.all()  
+    movies = Movie.objects.all()
     context = {
         'movies': movies
     }
     return render(request, 'pages/index.html', context)
 
 def movie_detail(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)   
+    movie = get_object_or_404(Movie, pk=pk)
     return render(request, 'pages/movie_detail.html', {'movie': movie})
-
 
 def about(request):
     return render(request, 'pages/about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            print("=== Новое сообщение ===")
+            print(f"Тема: {cleaned_data['subject']}")
+            print(f"Email: {cleaned_data['email']}")
+            print(f"Сообщение: {cleaned_data['text']}")
+            print("========================")
+            return redirect('home')
+    else:
+        form = FeedbackForm()
+    
+    return render(request, 'pages/contact.html', {'form': form})
