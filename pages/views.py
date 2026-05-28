de04bd3714e7d72bf9da72bf6e87f0a5f3bb8c0a
Alexry78 <144147456+Alexry78@users.noreply.github.com>
2026-05-28 16:22:33 +0300

    Task 6 ready: display movies from DB using QuerySet and loop

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from .models import Movie   
+from .models import Movie
 
 def index(request):
     movies = Movie.objects.all()  
-    return render(request, 'pages/index.html', {'movies': movies})
+    context = {
+        'movies': movies
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
