4538238d5a6466966c6850b76a4f2cc907a18795
Alexry78 <144147456+Alexry78@users.noreply.github.com>
2026-05-28 15:48:26 +0300

    Add Movie model, admin, migrations, env config with SQLite

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'director', 'release_year', 'rating', 'is_showing', 'created_at')
+    list_filter = ('is_showing', 'release_year')
+    search_fields = ('title', 'director')
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название фильма")
+    director = models.CharField(max_length=200, verbose_name="Режиссёр", blank=True)
+    description = models.TextField(verbose_name="Описание", blank=True)
+    release_year = models.IntegerField(verbose_name="Год выпуска", null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, verbose_name="Рейтинг", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+    is_showing = models.BooleanField(default=True, verbose_name="Сейчас в прокате")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
