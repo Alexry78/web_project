65386dbc69e46cb098040bcc72cf82da6096ba58
Alexry78 <144147456+Alexry78@users.noreply.github.com>
2026-05-28 14:07:36 +0300

    Add template inheritance, about page, dynamic context and named URLs

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {
+        'welcome_text': 'Добро пожаловать в наш сервис!',
+        'books': [
+            {'title': 'Книга 1', 'description': 'Описание первой книги'},
+            {'title': 'Книга 2', 'description': 'Описание второй книги'},
+            {'title': 'Книга 3', 'description': 'Описание третьей книги'},
+        ]
+    }
+    return render(request, 'pages/index.html', context)
+
+def about(request):
+    return render(request, 'pages/about.html')
