e31c117c9d28e1ee7ca1f742674bdce019257f91
Alexry78 <144147456+Alexry78@users.noreply.github.com>
2026-05-28 16:27:54 +0300

    Task 6 ready: display movies from DB with QuerySet and loop

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import index, about
-from django.conf import settings             
-from django.conf.urls.static import static   
+from pages.views import index, about   
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name='about'),
+    path('about/', about, name='about'),  
     path('admin/', admin.site.urls),
 ]
 
